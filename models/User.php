4c80e7dd90d09efc4898524d8406d021db7691f5
Mohamad Bashir Alsehaby <mbashiralsehaby@computest.nl>
2022-06-02 13:43:54 +0200

    code cleanup

--- models/User.php
+++ models/User.php
@@ -1,15 +1,6 @@
 <?php
     //Extend model class that perform all the connection to the database
     class User extends Model {
-        var $user_id;
-        var $username;
-        var $password;
-        var $role;
-
-        function __construct(){
-            parent::__construct();
-        }
-
         //Get users info
         public function get(){
             $SQL = "SELECT user_id,username,role FROM Users";
@@ -18,15 +9,7 @@
             $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
             return $stmt->fetchAll();
         }
-
-        //Update user password
-        public function updatePassword(){
-            $SQL = 'UPDATE Users SET password = :password WHERE username = :username';
-            $stmt = self::$_connection->prepare($SQL);
-            $stmt->execute(['password'=>$this->password, 'username'=>$this->username]);
-            return $stmt->rowCount();
-        }
-
+        
         //Find user bij username
         public function findUser($username){
             $SQL = 'SELECT * FROM Users WHERE username = :username';
@@ -36,7 +19,7 @@
             $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
             return $stmt->fetch();
         }
-
+        
         //Create user when register
         public function createUser(){
             $SQL = 'INSERT INTO Users(username, password) VALUES(:username, :password)';
@@ -44,6 +27,14 @@
             $stmt->execute(['username'=>$this->username, 'password'=>$this->password]);
             return $stmt->rowCount();
             echo 'Je hebt met succes aangemeld, log hier in om verder te gaan!';
+        }
+
+        //Update user password
+        public function updatePassword(){
+            $SQL = 'UPDATE Users SET password = :password WHERE username = :username';
+            $stmt = self::$_connection->prepare($SQL);
+            $stmt->execute(['password'=>$this->password, 'username'=>$this->username]);
+            return $stmt->rowCount();
         }
 
         //Delete user
